b55c06f502d5eb936a5c8740b9886414c4fabd75
DiegoBruno30 <diego_brm@windowslive.com>
2025-10-20 15:08:25 +0100

    feat(domain): validate weight > 0 for FoodItem

---
+++ StorageApp/src/cct/algorithms/storage/FoodItem.java
@@ -0,0 +1,40 @@
+/*Project:   CCT – Algorithms & Constructs (Storage)
+ Student:   Diego — ID 2025145 (2025145-afk)
+ Email:     2025145@student.cct.ie
+ Lecturer:  Taufique Ahmed
+ Date:      2025-10-17
+Course:    Higher diploma science Computing — Algorithms & Constructs
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cct.algorithms.storage;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+/**
+ *
+ * @author diego
+ */
+public class FoodItem {
+    
+      public final String name;
+    public final int weightGrams;
+    public final LocalDate bestBefore;      // hoje + 2 semanas (default)
+    public final LocalDateTime placedAt;    // carimbo de hora
+
+    public FoodItem(String name, int weightGrams) {
+        
+        if (weightGrams <= 0)
+        throw new IllegalArgumentException("Weight must be > 0");
+
+        this.name = name;
+        this.weightGrams = weightGrams;
+        this.bestBefore = LocalDate.now().plusWeeks(2);
+        this.placedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "FoodItem{name='" + name + "', weight=" + weightGrams +
+                "g, bestBefore=" + bestBefore + ", placedAt=" + placedAt + "}";
+    }
+}
